1941cd828eb0da46aafb688f3141d9b4d980ba93
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2017-05-17 10:15:19 -0400

    add heroku setings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(process.cwd() + '/public'));
