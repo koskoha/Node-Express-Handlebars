b8ef3578510420e59d50a8192fec1d619a13c112
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2017-05-17 23:32:28 -0400

    fix connection var for heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
-
+var connection;
 // MySql connection config
-module.exports = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: '',
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: '',
+        database: "burgers_db"
+    });
+}
+
+connection.connect();
+module.exports = connection;
